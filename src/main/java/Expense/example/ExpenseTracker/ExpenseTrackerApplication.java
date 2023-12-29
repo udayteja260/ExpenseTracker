dd96c78be81028828af46075769da73e5efef1e1
dineshkolla <Kolladinesh26@gmail.com>
2023-12-28 22:31:11 -0600

    Made code changes

--- src/main/java/Expense/example/ExpenseTracker/ExpenseTrackerApplication.java
+++ src/main/java/Expense/example/ExpenseTracker/ExpenseTrackerApplication.java
@@ -8,6 +8,7 @@ public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
+		System.out.println("Hi");
 	}
 
 }
